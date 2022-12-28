7708fcf93d60903163226bc5c972c060f84e156f
RHAtUAT <39317306+RHAtUAT@users.noreply.github.com>
2022-12-28 00:20:27 -0700

    Create RGBFade.cpp

---
+++ RGBFade.cpp
@@ -0,0 +1,19 @@
+	ImColor* RainbowFade()
+	{
+		auto frames = ImGui::GetFrameCount();
+		static int r = 255;
+		static int g = 0;
+		static int b = 0;
+
+		// Use modulus to check if it's divisible by 1, and if the remainder equals 0, then we continue. This effect gets called every frame.
+		if (frames % 1 == 0)
+		{
+			if (r == 255 && g < 255 && b == 0) { g++; }
+			else if (r > 0 && g == 255 && b == 0) { r--; }
+			else if (r == 0 && g == 255 && b < 255) { b++; }
+			else if (r == 0 && g > 0 && b == 255) { g--; }
+			else if (r < 255 && g == 0 && b == 255) { r++; }
+			else if (r == 255 && g == 0 && b > 0) { b--; }
+		}
+		return &ImColor(r, g, b, 255);
+	}
